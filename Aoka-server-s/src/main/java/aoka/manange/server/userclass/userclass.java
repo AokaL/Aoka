e1e17b47997e72f3cb9787289c6da9de59178d20
Aokali <yuanhaiyang9527@163.com>
2019-01-02 15:30:26 +0800

    实验代码能否导入

--- Aoka-server-s/src/main/java/aoka/manange/server/userclass/userclass.java
+++ Aoka-server-s/src/main/java/aoka/manange/server/userclass/userclass.java
@@ -4,7 +4,7 @@ package aoka.manange.server.userclass;
 public class userclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("nihao 15.29");
 
 	}
 
